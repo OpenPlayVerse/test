ecab07bd7078bdbabec57f775d8f82ba38bd277f
MisterNoNameLP <misternonamelp@web.de>
2023-03-16 20:42:25 +0100

    Pack update: test

--- serverStart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./java-8-openjdk/jre/bin/java -server -Xmx4G -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:MaxPermSize=256M -XX:+AggressiveOpts -jar forge-1.12.2-14.23.5.2860.jar nogui
